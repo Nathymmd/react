3d293f2aae748a9e23678e8462347ea92db41741
felipeleonn <felipeleon98@outlook.com>
2023-08-29 18:28:31 -0300

    finished all components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { SideBar, ContentWrapper } from './index'
 
-function App() {
+export default function App() {
   return (
-      <div id="root">
-          {/* <SideBar /> */}
-              <div className='wrapper'>
-                  <ContentWrapper />
-              </div>
-      </div>
+        <div id="root">
+            <SideBar />
+                <div className='wrapper'>
+                    <ContentWrapper />
+                </div>
+        </div>
   );
 }
 
-export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,17 @@ import SideBar from "./SideBar"
 import Footer from "./Footer"
 import ContentWrapper from "./ContentWrapper"
 import ContentRowTop from "./ContentRowTop"
+import ContentRowMovies from "./ContentRowMovies"
+import LastMovieInDb from "./LastMovieInDb"
+import GenresInDb from "./GenresInDb"
 
 export {
   TopBar,
   SideBar,
   Footer,
   ContentWrapper,
-  ContentRowTop
+  ContentRowTop,
+  ContentRowMovies,
+  LastMovieInDb,
+  GenresInDb
 };
